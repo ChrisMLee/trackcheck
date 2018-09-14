b043adee44e354d2d4b66a04bdd5433a5d602da7
Christopher Lee <cristovao.m.lee@gmail.com>
2018-09-13 23:19:40 -0400

    first commit

--- TrackCheck/ViewController.swift
+++ TrackCheck/ViewController.swift
@@ -7,6 +7,23 @@
 //
 
 import UIKit
+import Foundation
+
+enum Result<Value> {
+    case success(Value)
+    case failure(Error)
+}
+
+struct Post: Codable {
+    let userId: Int
+    let id: Int
+    let title: String
+    let body: String
+}
+
+func getPosts(for userId: Int, completion: ((Result<[Post]>) -> Void)?) {
+  
+}
 
 class ViewController: UIViewController {
 
@@ -20,6 +37,13 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func showAlert(){
+        print("hello")
+        let alert = UIAlertController(title: "hello world", message: "this is my first app!", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Awesome", style: .default, handler: nil)
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
 
 }
 
